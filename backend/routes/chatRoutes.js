4caf4ff51873ca844f970b593a891f497e2cc1fd
lifeconsciousness <lifeconsciousness321@gmail.com>
2023-05-07 22:49:17 +0200

    add and remove user from group chat API

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const { protect } = require('../middlewares/authMiddleware')
-const { accessChat, fetchChats, createGroupChat, renameGroupChat } = require('../controllers/chatControllers')
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroupChat,
+  addToGroupChat,
+  removeFromGroupChat,
+} = require('../controllers/chatControllers')
 
 const router = express.Router()
 
@@ -8,8 +15,8 @@ router.route('/').post(protect, accessChat)
 router.route('/').get(protect, fetchChats)
 router.route('/group').post(protect, createGroupChat)
 router.route('/renamegroup').put(protect, renameGroupChat)
-// router.route('/groupremoveuser').put(protect, removeUserGroupChat)
-// router.route('/groupadduser').put(protect, addUserGroupChat)
+router.route('/addtogroup').put(protect, addToGroupChat)
+router.route('/removefromgroup').put(protect, removeFromGroupChat)
 
 // router.route('/deletechat').put(protect, deleteChat) //use findByIdAndDelete
 
