6c6bf3bae10c38c9fc621e117a14ebf9137c98c3
lifeconsciousness <lifeconsciousness321@gmail.com>
2023-07-24 11:29:43 +0200

    origin url switch

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const server = app.listen(PORT, console.log(`Server stared on PORT ${PORT}`.yell
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:7000',
   },
 })
 
